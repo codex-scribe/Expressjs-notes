3f07e43ef0ee4d40113bf6711b27b804b6d5b1f6
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-10 01:01:06 -0500

    api notes request

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json')
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')))
 
 app.listen(PORT, () => console.log(`Note taking app listening at http://localhost:${PORT}.`));
