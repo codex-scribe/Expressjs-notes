7163de8b705d73f195541aff82897409ba9ac450
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-11 10:42:38 -0500

    connected post requests

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const db = require('./db/db.json')
 
 const app = express();
 const PORT = 3001;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} request received`);
+    const { title, text } = req.body;
+    console.log(title);
+    console.log(text)
+});
 
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')))
 
