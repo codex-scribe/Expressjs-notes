57020da78de39d2077890e49f6d7b8ae7d0690af
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-12 10:50:40 -0500

    completed post

--- server.js
+++ server.js
@@ -1,21 +1,59 @@
-const express = require('express');
-const path = require('path');
-const db = require('./db/db.json')
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.use(express.static("public"));
 
-app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`);
-    const { title, text } = req.body;
-    console.log(title);
-    console.log(text)
+//GET request for accessing the note page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+//POST request for writing a new note
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+  res.json(`${req.method} request received/json frontend`);
+  const { title, text } = req.body;
+  if (title && text) {
+  const newNote = { title, text };
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const savedNotes = JSON.parse(data);
+      savedNotes.push(newNote);
+      console.log(savedNotes);
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(savedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info("successfully added note!")
+      );
+    }
+  });
+  const response = {
+    status: "success",
+    body: "newReview",
+  };
+  console.log(response);
+  res.status(201).json(response);}
+  else {
+    res.status(500).json('Error in posting review')
+  }
 });
 
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')))
+//GET request for loading saved notes from the DB
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+);
 
-app.listen(PORT, () => console.log(`Note taking app listening at http://localhost:${PORT}.`));
+app.listen(PORT, () =>
+  console.log(`Note taking app listening at http://localhost:${PORT}.`)
+);
