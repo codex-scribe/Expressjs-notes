cf1ad117fc8dd46bbaaac007e665e9a04d7e2ccc
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-03-03 21:03:42 -0500

    updated port variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const { v4:generateId } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
